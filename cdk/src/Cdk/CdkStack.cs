b4e9a4107c7629b88edc2445ed408bbd870fa288
Andy Mehalick <andy.mehalick@outlook.com>
2022-11-21 08:33:00 -0500

    BT-3 add route53 and dns

--- cdk/src/Cdk/CdkStack.cs
+++ cdk/src/Cdk/CdkStack.cs
@@ -1,6 +1,9 @@
 using Amazon.CDK;
+using Amazon.CDK.AWS.CertificateManager;
 using Amazon.CDK.AWS.CloudFront;
 using Amazon.CDK.AWS.CloudFront.Origins;
+using Amazon.CDK.AWS.Route53;
+using Amazon.CDK.AWS.Route53.Targets;
 using Amazon.CDK.AWS.S3;
 using Constructs;
 
@@ -8,11 +11,41 @@ namespace Cdk;
 
 public class CdkStack : Stack
 {
+	private const string DomainName = "bananatracks.com";
+
 	internal CdkStack(Construct scope, string id, IStackProps props) : base(scope, id, props)
 	{
-		var bucket = new Bucket(this, Name("S3Bucket"), new BucketProps
+		var bucket = CreateS3Bucket();
+
+		var zone = new HostedZone(this, Name("HostedZone"), new HostedZoneProps
 		{
-			BucketName = "bananatracks.com",
+			ZoneName = DomainName
+		});
+
+		var certificate = new Certificate(this, Name("Certificate"), new CertificateProps
+		{
+			DomainName = DomainName,
+			SubjectAlternativeNames = new[] { "*.bananatracks.com" },
+			Validation = CertificateValidation.FromDns(zone)
+		});
+
+		var cloudFrontDistribution = CreateCloudFrontDistribution(bucket, certificate);
+
+		var cloudFrontTarget = RecordTarget.FromAlias(new CloudFrontTarget(cloudFrontDistribution));
+
+		var _ = new ARecord(this, Name("AppARecord"), new ARecordProps
+		{
+			Zone = zone,
+			RecordName = $"app.{DomainName}",
+			Target = cloudFrontTarget
+		});
+	}
+
+	private Bucket CreateS3Bucket()
+	{
+		return new(this, Name("S3Bucket"), new BucketProps
+		{
+			BucketName = DomainName,
 			BlockPublicAccess = new(new BlockPublicAccessOptions
 			{
 				BlockPublicAcls = false
@@ -22,11 +55,14 @@ public class CdkStack : Stack
 			WebsiteIndexDocument = "index.html",
 			WebsiteErrorDocument = "index.html"
 		});
+	}
 
+	private Distribution CreateCloudFrontDistribution(Bucket bucket, Certificate certificate)
+	{
 		var identity = new OriginAccessIdentity(this, Name("CloudFrontOriginAccessIdentity"));
 		bucket.GrantRead(identity);
 
-		var _ = new Distribution(this, Name("CloudFrontDistribution"), new DistributionProps
+		return new(this, Name("CloudFrontDistribution"), new DistributionProps
 		{
 			DefaultRootObject = "index.html",
 			DefaultBehavior = new BehaviorOptions
@@ -34,8 +70,13 @@ public class CdkStack : Stack
 				Origin = new S3Origin(bucket, new S3OriginProps
 				{
 					OriginAccessIdentity = identity
-				})
-			}
+				}),
+				ViewerProtocolPolicy = ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+				AllowedMethods = AllowedMethods.ALLOW_GET_HEAD
+			},
+			Certificate = certificate,
+			DomainNames = new[] { $"app.{DomainName}" },
+			HttpVersion = HttpVersion.HTTP2_AND_3
 		});
 	}
 

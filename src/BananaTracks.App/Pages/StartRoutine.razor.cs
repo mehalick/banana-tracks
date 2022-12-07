7f5ff6fe93f03f3b3d9c60c7fdb0e181bb5c893f
Andy Mehalick <andy.mehalick@outlook.com>
2022-12-07 12:20:36 -0500

    BT-7 refactor routine run

--- src/BananaTracks.App/Pages/StartRoutine.razor.cs
+++ src/BananaTracks.App/Pages/StartRoutine.razor.cs
@@ -2,6 +2,126 @@
 
 public partial class StartRoutine : AppComponentBase, IDisposable
 {
+	[Parameter]
+	public string RoutineId { get; set; } = default!;
+
+	private RoutineRun? _routineRun;
+
+	private enum RunStatus
+	{
+		IsPending, IsRunning, IsDone
+	}
+
+	private enum ActivityStatus
+	{
+		IsPending, IsRunning, IsBreaking, IsDone
+	}
+
+	protected override async Task OnInitializedAsync()
+	{
+		var response = await HttpClient.GetFromJsonAsync<GetRoutineByIdResponse>($"{ApiRoutes.GetRoutineById}?RoutineId={RoutineId}");
+
+		if (response is null)
+		{
+			throw new NullReferenceException($"Cannot find routine for ID '{RoutineId}'");
+		}
+
+		_routineRun = new(response.Routine);
+	}
+
+	private async Task RunTimer()
+	{
+		if (_routineRun is null)
+		{
+			return;
+		}
+
+		await _routineRun.Run(JsRuntime, async () => await InvokeAsync(StateHasChanged));
+	}
+
+	private static string DisplayTime(TimeSpan timeSpan)
+	{
+		if (timeSpan < TimeSpan.Zero)
+		{
+			return "00:00";
+		}
+
+		var ts = TimeSpan.FromSeconds(Math.Round(timeSpan.TotalSeconds));
+
+		return $"{ts.Minutes:00}:{ts.Seconds:00}";
+	}
+
+	public void Dispose() => _routineRun?.Dispose();
+
+	private class RoutineRun : IDisposable
+	{
+		public string Name { get; }
+		public RunStatus Status { get; private set; } = RunStatus.IsPending;
+		public List<ActivityRun> Activities { get; }
+
+		private readonly PeriodicTimer _timer = new(TimeSpan.FromSeconds(1));
+
+		public RoutineRun(RoutineModel routine)
+		{
+			Name = routine.Name;
+			Activities = routine.Activities.Select(ActivityRun.Create).ToList();
+		}
+
+		private void Start()
+		{
+			Console.WriteLine($"[RunTimer] Start routine: {Name}");
+
+			Status = RunStatus.IsRunning;
+		}
+
+		public async Task Run(IJSRuntime jsRuntime, Func<Task> onUpdate)
+		{
+			Start();
+
+			foreach (var activity in Activities)
+			{
+				await activity.StartActivity(jsRuntime);
+
+				while (await _timer.WaitForNextTickAsync())
+				{
+					var isComplete = await activity.UpdateDuration(jsRuntime);
+
+					await onUpdate();
+
+					if (isComplete)
+					{
+						break;
+					}
+				}
+
+				while (await _timer.WaitForNextTickAsync())
+				{
+					var isComplete = activity.UpdateBreak();
+
+					await onUpdate();
+
+					if (isComplete)
+					{
+						break;
+					}
+				}
+			}
+
+			await Complete(jsRuntime);
+		}
+
+		private async Task Complete(IJSRuntime jsRuntime)
+		{
+			Status = RunStatus.IsDone;
+
+			Console.WriteLine("[RunTimer] Play audio #audio-done");
+
+			await jsRuntime.InvokeAsync<string>("playAudio", "audio-done");
+		}
+
+		public void Dispose() => _timer.Dispose();
+	}
+
 	private class ActivityRun
 	{
 		public RoutineActivityModel Activity { get; }
@@ -11,6 +131,8 @@ public partial class StartRoutine : AppComponentBase, IDisposable
 
 		private readonly TimeSpan _durationTime;
 		private readonly TimeSpan _breakTime;
+		private DateTime? _durationStart;
+		private DateTime? _breakState;
 
 		private ActivityRun(RoutineActivityModel routineActivity)
 		{
@@ -28,26 +150,40 @@ public partial class StartRoutine : AppComponentBase, IDisposable
 			return new(activity);
 		}
 
-		private DateTime? _durationStart;
-		private DateTime? _breakState;
+		public async Task StartActivity(IJSRuntime jsRuntime)
+		{
+			Console.WriteLine($"[RunTimer] Start activity: {Activity.Name}");
+
+			Status = ActivityStatus.IsRunning;
+
+			if (!string.IsNullOrWhiteSpace(Activity.AudioUrl))
+			{
+				Console.WriteLine($"[RunTimer] Play audio #audio-{Activity.ActivityId}");
+
+				await jsRuntime.InvokeAsync<string>("playAudio", $"audio-{Activity.ActivityId}");
+			}
+		}
 
 		public async Task<bool> UpdateDuration(IJSRuntime jsRuntime)
 		{
 			_durationStart ??= DateTime.Now;
 
-			Status = ActivityStatus.IsRunning;
 			DurationRemaining = _durationTime.Subtract(DateTime.Now.Subtract(_durationStart.Value));
 
 			if (DurationRemaining.Ticks > 0)
 			{
 				return false;
 			}
-			
-			if (_breakTime != TimeSpan.Zero)
+
+			if (_breakTime == TimeSpan.Zero)
 			{
-				await jsRuntime.InvokeAsync<string>("playAudio", "audio-break");
+				return true;
 			}
-				
+
+			Console.WriteLine("[RunTimer] Play audio #audio-break");
+
+			await jsRuntime.InvokeAsync<string>("playAudio", "audio-break");
+
 			return true;
 		}
 
@@ -65,106 +201,14 @@ public partial class StartRoutine : AppComponentBase, IDisposable
 
 			BreakRemaining = _breakTime.Subtract(DateTime.Now.Subtract(_breakState.Value));
 
-			if (BreakRemaining <= TimeSpan.Zero)
+			if (BreakRemaining > TimeSpan.Zero)
 			{
-				Status = ActivityStatus.IsDone;
-				return true;
+				return false;
 			}
 
-			return false;
+			Status = ActivityStatus.IsDone;
+
+			return true;
 		}
 	}
-
-	[Parameter]
-	public string RoutineId { get; set; } = default!;
-
-	private readonly PeriodicTimer _timer = new(TimeSpan.FromSeconds(1));
-	private RoutineModel? _routine;
-	private List<ActivityRun> _activities = new();
-
-	private RunStatus _runStatus = RunStatus.IsPending;
-
-	private enum RunStatus
-	{
-		IsPending, IsRunning, IsDone
-	}
-
-	private enum ActivityStatus
-	{
-		IsPending, IsRunning, IsBreaking, IsDone
-	}
-
-	protected override async Task OnInitializedAsync()
-	{
-		var response = await HttpClient.GetFromJsonAsync<GetRoutineByIdResponse>($"{ApiRoutes.GetRoutineById}?RoutineId={RoutineId}");
-
-		_routine = response?.Routine!;
-		_activities = _routine.Activities.Select(ActivityRun.Create).ToList();
-	}
-
-	private async Task StartTimer()
-	{
-		_runStatus = RunStatus.IsRunning;
-
-		Console.WriteLine("[StartTimer] Start");
-
-		if (_routine is null)
-		{
-			return;
-		}
-
-		Console.WriteLine($"[StartTimer] Routine: {_routine.Name}");
-
-		foreach (var activity in _activities)
-		{
-			Console.WriteLine($"[StartTimer] Activity: {activity.Activity.Name}");
-
-			if (!string.IsNullOrWhiteSpace(activity.Activity.AudioUrl))
-			{
-				await JsRuntime.InvokeAsync<string>("playAudio", $"audio-{activity.Activity.ActivityId}");
-			}
-
-			while (await _timer.WaitForNextTickAsync())
-			{
-				var isComplete = await activity.UpdateDuration(JsRuntime);
-
-				await InvokeAsync(StateHasChanged);
-
-				if (isComplete)
-				{
-					break;
-				}
-			}
-
-			while (await _timer.WaitForNextTickAsync())
-			{
-				var isComplete = activity.UpdateBreak();
-
-				await InvokeAsync(StateHasChanged);
-
-				if (isComplete)
-				{
-					break;
-				}
-			}
-		}
-
-		_runStatus = RunStatus.IsDone;
-
-		await JsRuntime.InvokeAsync<string>("playAudio", "audio-done");
-	}
-
-	private static string DisplayTime(TimeSpan timeSpan)
-	{
-		if (timeSpan < TimeSpan.Zero)
-		{
-			return "00:00";
-		}
-
-		var ts = TimeSpan.FromSeconds(Math.Round(timeSpan.TotalSeconds));
-
-		return $"{ts.Minutes:00}:{ts.Seconds:00}";
-	}
-
-	public void Dispose() => _timer.Dispose();
 }

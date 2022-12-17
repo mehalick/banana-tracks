3614595cb37a865170a24b94a8df3237c969c58f
Andy Mehalick <andy.mehalick@outlook.com>
2022-12-17 11:43:06 -0500

    Revert "BT-16 remove references"
    This reverts commit d0951c1760e249cd4adbc42edede76d344aa52ef.

--- src/BananaTracks.Functions.SessionSaved/Function.cs
+++ src/BananaTracks.Functions.SessionSaved/Function.cs
@@ -1,17 +1,23 @@
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
 using Amazon.Lambda.Core;
+using Amazon.Lambda.SQSEvents;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using Amazon.Lambda.RuntimeSupport;
 using Amazon.Lambda.Serialization.SystemTextJson;
-using System.Text.Json.Serialization;
+using BananaTracks.Core.Configuration;
+using BananaTracks.Core.Entities;
 
 namespace BananaTracks.Functions.SessionSaved;
 
 public class Function
 {
-	//private readonly IDynamoDBContext _dynamoDbContext;
+	private readonly IDynamoDBContext _dynamoDbContext;
 
 	public Function()
 	{
-		//_dynamoDbContext = new DynamoDBContext(new AmazonDynamoDBClient());
+		_dynamoDbContext = new DynamoDBContext(new AmazonDynamoDBClient());
 	}
 
 	private static async Task Main()
@@ -22,32 +28,32 @@ public class Function
 			.RunAsync();
 	}
 
-	public async Task FunctionHandler(string sqsEvent, ILambdaContext context)
+	public async Task FunctionHandler(SQSEvent sqsEvent, ILambdaContext context)
 	{
-		//foreach (var record in sqsEvent.Records)
-		//{
-		//	await ProcessRecordAsync(record, context);
-		//}
+		foreach (var record in sqsEvent.Records)
+		{
+			await ProcessRecordAsync(record, context);
+		}
 	}
 
-	//private async Task ProcessRecordAsync(SQSEvent.SQSMessage message, ILambdaContext context)
-	//{
-	//	var body = JsonSerializer.Deserialize(message.Body, AppJsonSerializerContext.Default.SessionSavedMessage)!;
+	private async Task ProcessRecordAsync(SQSEvent.SQSMessage message, ILambdaContext context)
+	{
+		var body = JsonSerializer.Deserialize(message.Body, AppJsonSerializerContext.Default.SessionSavedMessage)!;
 
-	//	context.Logger.LogInformation($"Processed activity UserId: {body.UserId} ActivityId: {body.RoutineId}");
+		context.Logger.LogInformation($"Processed activity UserId: {body.UserId} ActivityId: {body.RoutineId}");
 
-	//	await _dynamoDbContext.SaveAsync(new Session
-	//	{
-	//		UserId = body.UserId,
-	//		RoutineId = body.RoutineId
-	//	});
+		await _dynamoDbContext.SaveAsync(new Session
+		{
+			UserId = body.UserId,
+			RoutineId = body.RoutineId
+		});
 
-	//	await Task.CompletedTask;
-	//}
+		await Task.CompletedTask;
+	}
 }
 
-//[JsonSerializable(typeof(SQSEvent))]
-[JsonSerializable(typeof(string))]
+[JsonSerializable(typeof(SQSEvent))]
 public partial class LambdaFunctionJsonSerializerContext : JsonSerializerContext
 {
 }
+
